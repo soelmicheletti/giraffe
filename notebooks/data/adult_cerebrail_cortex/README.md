d1d41b9bdd8b17d66ef9a66bcac7ca672d02a7ec
Soel Micheletti <soel.micheletti@hotmail.it>
2023-02-17 08:06:37 +0100

    Finished preprocessing

--- notebooks/data/adult_cerebrail_cortex/README.md
+++ notebooks/data/adult_cerebrail_cortex/README.md
@@ -1,5 +1,24 @@
-# ATAC-seq
+# Chromatin accessibility on mus musculus
 
-Raw data in `raw` have been downloaded from the following paths in [Netbooks](http://netbooks.networkmedicine.org/hub/login) (see the also the [GitHub repo](https://github.com/netZoo/netbooks):
+Analysis of simultaneous profiling of transcriptome and chromatin accessibility within single cells based on data provided in the paper
 
-https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE162515
+*An ultra high-throughput method for single-cell joint analysis of open chromatin and transcriptome* by C. Zhu et al. 
+
+The organism under study is [mus musculus](https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=10090), with the reference genome GRCm38. Here a review of the data in `raw`:
+
+- Data in `raw/Adult_CTX_DNA` and `raw/Adult_CTX_RNA` can be found on NCBI Gene Expression Omnibus (GEO) (http://www.ncbi.nlm.nih.gov/geo/) under accession number [GSE130399](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE130399). Since data are sparse, they are conveniently stored in  [mtx format](https://math.nist.gov/MatrixMarket/formats.html) to save memory. 
+   - `raw/Adult_CTX_DNA/matrix.mtx` contains the cell-specific number of counts in every region of the genome.  The cell identifiers are to be found in `raw/Adult_CTX_DNA/barcodes.tsv`), and the genome regions (indexed by chromosome and an offset) are in `raw/Adult_CTX_DNA/raw/Adult_CTX_DNA/genes.tsv`.
+   - `raw/Adult_CTX_RNA/matrix.mtx` contains the cell-specific gene expression for each protein coding gene in mice. The genes names are saved in `raw/Adult_CTX_DNA/barcodes.tsv` (both ensembl ID and alias), while the cell identifiers (that correspond to the ones in the bullet point above) are in `raw/Adult_CTX_RNA/barcodes.tsv`. 
+- Data in `raw/mart_export.txt` contains the location of each gene (including chromosome and strand). This dataset was collected from [ENSEMBL](http://useast.ensembl.org/biomart/martview/) by selecting multiple attributes (Gene stable ID, Gene stable ID version, Transcript stable ID , Transcript stable ID version, Gene start (bp), Gene end (bp), Ensembl Canonical, Gene type, Gene name, Strand, Chromosome/scaffold name) for mouse genes (GRCm38). 
+- The PPI is taken from [MIPPIE](http://cbdm-01.zdv.uni-mainz.de/~galanisl/mippie/download.php), a project for mouse integrated protein-protein interaction reference. 
+   - `raw/mippie_ppi_v1_0.tsv` contains 42,610 interactions between 10,886 proteins. 
+   - `mippie_proteins_v1_0.tsv` contains the translation between entrez and protein number. 
+- The motif is taken from [here](https://sites.google.com/a/channing.harvard.edu/kimberlyglass/tools/resources?authuser=0), where data have properly been preprocessed for version GRCm38. In particular, the mappings were produced by using FIMO to scan a genome for sets of  CIS-BP Single Species DNA-motifs curated in the MEME suite. 
+    - `raw/mm10_reseq_-750_250_1e-4.txt` contains the edges in our TF-edge prior. 
+    - `raw/Mus_musculus_motifinfo.txt` contains the mapping to the TF names. 
+
+In `preprocessing.py` we prepare the data that we will use in our analysis. In particular:
+
+- We transform the motif to an adjacency matrix, which is the format required by GIRAFFE. 
+- We extract the PPI in adjacency matrix form, using a consistent naming with the motif file.
+- For each gene and each cell, we save the number of counts in the promoter region. 
--- notebooks/data/adult_cerebrail_cortex/preprocessing.py
+++ notebooks/data/adult_cerebrail_cortex/preprocessing.py
@@ -3,6 +3,12 @@ import requests
 from scipy.io import mmread
 
 def binarySearch(data, val):
+    """
+    Performs binary search to find indices of a gene promoter region in the chromatine dataset
+    :param data: dataframe containing ATAC-seq data in every region of the genome
+    :param val: region to be searched
+    :return: index of region in data
+    """
     lo, hi = 0, len(data) - 1
     best_ind = lo
     while lo <= hi:
@@ -14,66 +20,18 @@ def binarySearch(data, val):
         else:
             best_ind = mid
             break
-        # check if data[mid] is closer to val than data[best_ind]
+        if mid + 1 < len(data) and data[mid] < val and data[mid + 1] >= val:
+            return mid
         if abs(data[mid] - val) < abs(data[best_ind] - val):
             best_ind = mid
     return best_ind
 
-def generate_data():
-    genes = pd.read_csv('data/adult_cerebrail_cortex/raw/Adult_CTX_RNA/genes.tsv', header=None, sep='\t')
-
-    chromatine = mmread('data/adult_cerebrail_cortex/raw/Adult_CTX_DNA/matrix.mtx')
-    rows = pd.read_csv('data/adult_cerebrail_cortex/raw/Adult_CTX_DNA/genes.tsv', header=None, sep='\t')
-
-    gene_locations = pd.DataFrame(0, index = genes[0], columns = ['gene_name', 'chr', 'start', 'end'])
-    gene_locations['gene_name'] = list(genes[1])
-    starts = []
-    ends = []
-    c = []
-    for g in genes[0][len(starts):]:
-        print(g)
-        r = requests.get("https://useast.ensembl.org/Mus_musculus/Gene/Summary?g=" + str(g))
-        a = str(r.content)
-        b = a.split("r=", 1)[1]
-        chromosome = b.split(":", 1)[0]
-        start = b.split(":", 1)[1].split("-", 1)[0]
-        end = b.split(":", 1)[1].split("-", 1)[1].split("\"", 1)[0]
-        if ';' in end:
-            end = end.split(";", 1)[0]
-        c.append(chromosome)
-        starts.append(start)
-        ends.append(end)
-        gene_locations.loc[g, 'chr'] = chromosome
-        gene_locations.loc[g, 'start'] = start
-        gene_locations.loc[g, 'end'] = end
-
-    gene_locations.to_csv("data/adult_cerebrail_cortex/gene_locations.csv")
-
-    chr = []
-    start = []
-    end = []
-    for entry in rows[0]:
-        chr.append(entry.split(":", 1)[0])
-        start.append(int(entry.split(":", 1)[1].split("-", 1)[0]))
-        end.append(int(entry.split(":", 1)[1].split("-", 1)[1]))
-    rows_processed = pd.DataFrame(0, index=rows.index, columns=["chr", "start", "end"])
-    rows_processed['chr'] = chr
-    rows_processed['start'] = start
-    rows_processed['end'] = end
-    access = []
-    for i in range(len(genes)):
-        if i % 500 == 0:
-            print(i)
-        gene_info = gene_locations.loc[genes[0][i]]
-        chromosome = gene_info['chr']
-        start = gene_info['start']
-        if not start.isdigit():
-            access.append(-1)
-            continue
-        idx = binarySearch(list(rows_processed[rows_processed['chr'] == 'chr' + str(chromosome)]['start']), int(start))
-        access.append(np.sum(chromatine.getrow(idx).toarray()[0]))
-    pd.DataFrame(access).to_csv("data/adult_cerebrail_cortex/chromatine.txt")
-
+def get_motif(genes):
+    """
+    Transform list of motif edges to an adjacency matrix
+    :param genes: dataframe containing gene names
+    :return: motif matrix
+    """
     motif = pd.read_csv("data/adult_cerebrail_cortex/raw/mm10_reseq_-750_250_1e-4.txt", sep='\t', header=None)
     motif_matrix = pd.DataFrame(0, index=genes[1], columns=set(motif[0]))
     motif = motif[motif[2] == 1]
@@ -82,19 +40,25 @@ def generate_data():
         g = motif[1][i]
         if g in set(genes[1]):
             motif_matrix.loc[g, tf] = 1
-    motif_matrix.to_csv("data/adult_cerebrail_cortex/motif.txt")
-    prot_info = pd.read_csv("data/adult_cerebrail_cortex/raw/mippie_proteins_v1_0.tsv", sep='\t')
+
+def get_ppi(motif):
+    """
+    Extract the ppi network from
+    :param motif: dataframe of dimension G x TF containing the prior to the regulatory network
+    :return: ppi network as dataframe of dimension TF x TF
+    """
+    name_to_entrez = pd.read_csv("data/adult_cerebrail_cortex/raw/mippie_proteins_v1_0.tsv", sep='\t')
     ppi_list = pd.read_csv("data/adult_cerebrail_cortex/raw/mippie_ppi_v1_0.tsv", sep='\t')
-    translate = pd.read_csv("data/adult_cerebrail_cortex/raw/Mus_musculus_motifinfo.txt", sep='\t')
-    ppi = pd.DataFrame(0, index=motif_matrix.columns, columns=motif_matrix.columns)
+    id_to_name = pd.read_csv("data/adult_cerebrail_cortex/raw/Mus_musculus_motifinfo.txt", sep='\t')
+    ppi = pd.DataFrame(0, index = motif.columns, columns = motif.columns)
     for i in ppi.columns:
         for j in ppi.columns:
-            if i + "_1.02" in set(translate['MotifID']) and j + "_1.02" in set(translate['MotifID']):
-                tf1 = translate[translate['MotifID'] == i + "_1.02"]['TF_Info'].values[0]
-                tf2 = translate[translate['MotifID'] == j + "_1.02"]['TF_Info'].values[0]
-                if tf1 in set(prot_info['official_symbol']) and tf2 in set(prot_info['official_symbol']):
-                    entrez1 = prot_info[prot_info['official_symbol'] == tf1]['entrez'].values[0]
-                    entrez2 = prot_info[prot_info['official_symbol'] == tf2]['entrez'].values[0]
+            if i + "_1.02" in set(id_to_name['MotifID']) and j + "_1.02" in set(id_to_name['MotifID']):
+                tf1 = id_to_name[id_to_name['MotifID'] == i + "_1.02"]['TF_Info'].values[0]
+                tf2 = id_to_name[id_to_name['MotifID'] == j + "_1.02"]['TF_Info'].values[0]
+                if tf1 in set(name_to_entrez['official_symbol']) and tf2 in set(name_to_entrez['official_symbol']):
+                    entrez1 = name_to_entrez[name_to_entrez['official_symbol'] == tf1]['entrez'].values[0]
+                    entrez2 = name_to_entrez[name_to_entrez['official_symbol'] == tf2]['entrez'].values[0]
                     if entrez1 in set(ppi_list['entrezA']) and entrez2 in set(ppi_list['entrezB']):
                         tmp = ppi_list[ppi_list['entrezA'] == entrez1]
                         val = tmp[tmp['entrezB'] == entrez2]['MIPPIE_score'].values
@@ -107,3 +71,102 @@ def generate_data():
                         if val >= 0.4:
                             ppi.loc[i, j] = 1
                             ppi.loc[j, i] = 1
+    return ppi
+
+def save_gene_accessbility(genes, rows, chromatine):
+    """
+    Saves number of counts per cell for every gene
+    :param genes: list of gene names
+    :param rows: list of gene regions in the chromatine dataset
+    :param chromatine: counts in the dataset
+    """
+    chr = []
+    start = []
+    end = []
+    for entry in rows[0]:
+        chr.append(entry.split(":", 1)[0])
+        start.append(int(entry.split(":", 1)[1].split("-", 1)[0]))
+        end.append(int(entry.split(":", 1)[1].split("-", 1)[1]))
+    rows_processed = pd.DataFrame(0, index=rows.index, columns=["chr", "start", "end"])
+    rows_processed['chr'] = chr
+    rows_processed['start'] = start
+    rows_processed['end'] = end
+
+    cells = pd.read_csv('data/adult_cerebrail_cortex/raw/Adult_CTX_DNA/barcodes.tsv', header=None)
+    gene_locations = pd.read_csv("data/adult_cerebrail_cortex/raw/mart_export.txt", sep="\t", index_col=0)
+    access = []
+    to_drop = []
+
+    for i in range(len(genes)):
+        if i % 500 == 0:
+            print(i)
+        if i % 1000 == 0:
+            idx = [j for j in range(i)]
+            idx = list(set(idx) - set(to_drop))
+            pd.DataFrame(access, index=genes[0][idx]).to_csv("data/adult_cerebrail_cortex/accessibility.txt")
+        if genes[0][i] not in set(gene_locations.index):
+            to_drop.append(i)
+            continue
+        gene_info = gene_locations.loc[genes[0][i]]
+        strand = gene_info['Strand']
+        chromosome = gene_info['Chromosome/scaffold name']
+        if isinstance(chromosome, pd.core.series.Series):
+            chromosome = chromosome.values[0]
+        start_list = list(rows_processed[rows_processed['chr'] == 'chr' + str(chromosome)]['start'])
+        if (isinstance(gene_locations.loc[genes[0][i]]['Strand'], np.int64) and strand == 1) or (
+                not isinstance(gene_locations.loc[genes[0][i]]['Strand'], np.int64) and strand.values[0] == 1):
+            start = gene_info['Gene start (bp)']
+            if isinstance(start, pd.core.series.Series):
+                start = start.values[0]
+            start_index = binarySearch(start_list, start - 500)
+            end_index = binarySearch(start_list, start)
+            res = [0 for i in range(len(cells))]
+            for idx in range(start_index, end_index + 1):
+                if idx >= len(start_list):
+                    continue
+                key = "chr" + str(chromosome) + ":" + str(start_list[idx]) + "-" + str(start_list[idx] + 1000)
+                offset = list(rows[0]).index(key)
+                if idx != start_index and idx != end_index:
+                    res += chromatine.getrow(offset).toarray()[0]
+                elif idx == start_index:
+                    res += chromatine.getrow(offset).toarray()[0] * (1000 - ((start - 500) % 1000)) / 1000
+                elif idx == end_index:
+                    res += chromatine.getrow(offset).toarray()[0] * (start % 1000) / 1000
+            access.append(res)
+        else:
+            end = gene_info['Gene end (bp)']
+            if isinstance(end, pd.core.series.Series):
+                end = end.values[0]
+            start_index = binarySearch(start_list, end)
+            end_index = binarySearch(start_list, end + 500)
+            res = [0 for i in range(len(cells))]
+            for idx in range(start_index, end_index + 1):
+                if idx >= len(start_list):
+                    continue
+                key = "chr" + str(chromosome) + ":" + str(start_list[idx]) + "-" + str(start_list[idx] + 1000)
+                offset = list(rows[0]).index(key)
+                if idx != start_index and idx != end_index:
+                    res += chromatine.getrow(offset).toarray()[0]
+                elif idx == start_index:
+                    res += chromatine.getrow(offset).toarray()[0] * (1000 - (end % 1000)) / 1000
+                elif idx == end_index:
+                    res += chromatine.getrow(offset).toarray()[0] * ((end + 500) % 1000) / 1000
+            access.append(res)
+    idx = [j for j in range(len(genes))]
+    idx = list(set(idx) - set(to_drop))
+    pd.DataFrame(access, index=genes[0][idx]).to_csv("data/adult_cerebrail_cortex/accessibility.txt")
+
+def generate_data():
+    genes = pd.read_csv('data/adult_cerebrail_cortex/raw/Adult_CTX_RNA/genes.tsv', header=None, sep='\t')
+
+    chromatine = mmread('data/adult_cerebrail_cortex/raw/Adult_CTX_DNA/matrix.mtx')
+    rows = pd.read_csv('data/adult_cerebrail_cortex/raw/Adult_CTX_DNA/genes.tsv', header=None, sep='\t')
+
+    save_gene_accessbility(genes, rows, chromatine)
+
+    motif = get_motif(genes)
+    motif.to_csv("data/adult_cerebrail_cortex/motif.txt")
+
+    ppi = get_ppi(motif)
+    ppi.to_csv("data/adult_cerebrail_cortex/ppi.txt")
+
